b5b77e598ae445f671717f836cc07490bea38ec7
user.email <pavelbahutski@gmail.com>
2021-03-14 20:44:55 +0300

    Complete Task 3

---
+++ src/ua/senla/task4/Main.java
@@ -0,0 +1,19 @@
+package ua.senla.task4;
+
+public class Main {
+
+    public static void main (String [] args ){
+
+        ArraysMyLearn arr = new ArraysMyLearn();
+
+        //arr.method();
+        //arr.print();
+        //arr.randomString();
+        //arr.BinSearch();
+
+        WorkingArr work = new WorkingArr();
+
+        work.method();
+    }
+
+}
